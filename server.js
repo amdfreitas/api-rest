6b06f67090f6773c96686e944e0a34ac16243e10
Anderson Menezes de Freitas <andersonmfreitas25@gmail.com>
2021-05-15 22:53:23 -0300

    get resposta

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const consign  = require('consign');
 const app = express();
+
+consign({cwd:'src'})
+    .include('controller')
+    .then('routas')
+    .into(app);
+
+
 
 
 module.exports = app;
