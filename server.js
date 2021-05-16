4203e0b9d65a6534b9dee1f0d697c0c83ff81b49
Anderson Menezes de Freitas <andersonmfreitas25@gmail.com>
2021-05-15 23:59:57 -0300

    json body

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const consign  = require('consign');
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
 consign({cwd:'src'})
     .include('controller')
     .then('routas')
