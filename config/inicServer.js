d1bfd2e44a4a7dd6af0689bd21e64ddea0ef53f3
Anderson Menezes de Freitas <andersonmfreitas25@gmail.com>
2021-05-22 13:25:46 -0300

    crud_completo1

--- config/inicServer.js
+++ config/inicServer.js
@@ -1,10 +1,7 @@
 const app = require('../server');
 const  server = require('http');
 
-  
 const inicia = server.createServer(app);
-
-
 
 inicia.listen(3000,() => {
     console.log('Servidor esta UP !');
