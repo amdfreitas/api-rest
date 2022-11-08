44a48065682ea44e86f93f4b69a5b4b3b77b9796
amdfreitas <andersonmfreitas25@gmail.com>
2022-11-07 22:08:12 -0300

    config eslint

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-const express = require('express');
-const consign  = require('consign');
+const express = require("express");
+const consign = require("consign");
 const app = express();
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-consign({cwd:'src'})
-    .include('db')
-    .then('model')
-    .then('controller')
-    .then('routas')
-    .into(app);
-
-
-
+consign({ cwd: "src" })
+  .include("db")
+  .then("model")
+  .then("controller")
+  .then("routas")
+  .into(app);
 
 module.exports = app;
--- src/routas/produto.js
+++ src/routas/produto.js
@@ -1,14 +1,10 @@
-
 module.exports = (app) => {
+  const produt = app.controller.controlproduto;
+  //configuração  da rotas de produto.
 
-    const produt = app.controller.controlproduto;
-    //configuração  da rotas de produto.
-    
-    app.post('/cadastra/produto',produt.cadProduto);
-    app.put('/update/produto/',produt.updateProduto);
-    app.get('/search/produto/:id',produt.searchProduto);
-    app.get('/all/produto/',produt.allProduto);
-    app.delete('/delete/produto/',produt.deleteProduto);
-    
-
-}
+  app.post("/cadastra/produto", produt.cadProduto);
+  app.put("/update/produto/", produt.updateProduto);
+  app.get("/search/produto/:id", produt.searchProduto);
+  app.get("/all/produto/", produt.allProduto);
+  app.delete("/delete/produto/", produt.deleteProduto);
+};
